95eaf150d58c4a06acea5b78a104466f0b4937b4
LGLopez <darkdestruktion@gmail.com>
2020-08-04 23:42:22 -0500

    Para deploy

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import 'tachyons';
+import * as serviceWorker from './serviceWorker';
+
+//This will be the main render of React, 
+ReactDOM.render(
+  <App/>
+  ,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
