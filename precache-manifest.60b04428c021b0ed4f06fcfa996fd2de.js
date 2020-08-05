c77f9912e49b3cb0d87b797f541274e6871be70b
LGLopez <darkdestruktion@gmail.com>
2020-08-04 23:46:50 -0500

    Updates

---
+++ precache-manifest.60b04428c021b0ed4f06fcfa996fd2de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f048c823a76e3ecd9e724b8da5ff676d",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "06c64630a6390e66d0e3",
+    "url": "/RoboFriends/static/css/main.a8710369.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c64630a6390e66d0e3",
+    "url": "/RoboFriends/static/js/main.efcc6622.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
